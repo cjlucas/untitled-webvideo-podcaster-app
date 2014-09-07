7a353d54747bb29d70ca2bbb9c0ceec7359af223
Chris Lucas <chris@chrisjlucas.com>
2014-09-07 15:16:22 -0700

    Return 403 when trying to access api w/o creds

--- api/policies/requireCurrentUser.js
+++ api/policies/requireCurrentUser.js
@@ -1,5 +1,9 @@
 module.exports = function(req, res, next) {
   if (this.currentUser == null) {
+    // don't redirect to login if trying to access api
+    if (req.path.match(/^\/api\//)) {
+      return res.status(403).send('Permission denied');
+    }
     return res.redirect('/login')
   }
 
