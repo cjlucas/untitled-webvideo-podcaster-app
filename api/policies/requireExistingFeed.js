d67be1bf7235e06580709a3fdba8abf321ba6272
Chris Lucas <chris@chrisjlucas.com>
2014-09-16 17:22:47 -0700

    Return 400 if invalid feed id is given

--- api/policies/requireExistingFeed.js
+++ api/policies/requireExistingFeed.js
@@ -6,8 +6,8 @@
  * @note This policy should be used in conjunction with requireIdParameter.
  */
 module.exports = function(req, res, next) {
-  Feed.findOneById(req.param('id'), function(exec, feed) {
-    if (err) return res.status(500).json({dbError: err});
+  Feed.findById(req.param('id'), function(err, feed) {
+    if (err) return res.status(400).json({dbError: err});
     if (!feed) return res.status(404).json({error: 'Feed not found'});
 
     req.feed = feed;
